3e9e69678bba170d98573945fb4153598237942a
Peter Ting <pting1995@gmail.com>
2021-02-20 15:30:14 -0800

    added a clicked key to each picture for the onclick logic and started on a onlick score incrementer

--- src/components/CatCard/index.js
+++ src/components/CatCard/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./style.css";
 
+function clickChoice(id) {
+  
+}
+
 function catCard(props) {
   return (
     <div className="col-md-3">
       
-        <img src={props.pic} alt={props.name} className="rounded" width="250" height="250" />
+        <img src={props.pic} alt={props.name} className="rounded" width="250" height="250" onClick={() => clickChoice(this.props.id)} />
       {/* make onclick for the image */}
     </div>
     
